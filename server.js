0e3ba8b079afd632c0f323c1ba4c726d77f6b4a4
Dorian Legros <d.ratha76@hotmail.com>
2020-02-05 14:59:38 +0100

    api architecture + some dependancies installed

---
+++ server.js
@@ -0,0 +1,17 @@
+const Express = require('./node_modules/express');
+const Mongoose = require('./node_modules/mongoose');
+
+const app = Express();
+
+// avoid CORS policy
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// connection to MongoDB
+Mongoose
+    .connect('dbURI', { useNewUrlParser: true })
+    .then(() => console.log('Connexion à la base de données réussie !'))
+    .catch(err => console.log(err));
