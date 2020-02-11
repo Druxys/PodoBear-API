8dc8ebbf33b7aa119a9159966ddb1b217dd2db67
Druxys <drudrux@gmail.com>
2020-02-11 10:27:24 +0100

    improve server.js

--- server.js
+++ server.js
@@ -1,26 +1,8 @@
-const Express = require('express');
-const Mongoose = require('mongoose');
-const BodyParser = require('body-parser');
+const http = require('http');
+const app = require('./app');
 
 const port = 3000;
 
-const app = Express();
-app.use(BodyParser.urlencoded({ extended: true }));
-app.use(BodyParser.json());
+const server = http.createServer(app);
 
-// avoid CORS policy
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
-// connection to MongoDB
-Mongoose
-    .connect('mongodb+srv://Podobear:6Ab3TVcdCjKtEQT@cluster0-86rki.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connexion à la base de données réussie !'))
-    .catch(err => console.log(err));
-
-app.use('/datas', require('./routes/datas'));
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+server.listen(port);
