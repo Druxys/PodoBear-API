c6d596dda9f7dcf4cb350b02aa7ab30feefc4e31
Armand_D <armand.deshais@mail.com>
2020-03-09 14:46:44 +0100

    oui

--- Middleware/check-auth.js
+++ Middleware/check-auth.js
@@ -4,6 +4,7 @@ module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
         req.userData = jwt.verify(token, process.env.JWT_KEY);
+        console.log(process.env.JWT_KEY);
         next();
     } catch (error) {
         return res.status(401).json({
