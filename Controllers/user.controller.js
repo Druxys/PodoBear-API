ec81b0e1c58ddb9aba5ccffebbb9e34e64cd2671
Dorian Legros <d.ratha76@hotmail.com>
2020-03-11 11:18:18 +0100

    revomed moment dependancy in user.controller.js

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -7,7 +7,6 @@ const checkAuth = require('../Middleware/check-auth');
 
 const User = require("../Models/users.model");
 const Datas = require('../Models/datas.model');
-var moment = require('moment');
 
 exports.user_signup = (req, res, next) => {
     User.find({email: req.body.email})
